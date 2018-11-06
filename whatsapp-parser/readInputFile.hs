b281fc598b055e98342061ceb74ae4ecd895c3d4
ChavaO777 <salvador@orozco.in>
2018-11-06 15:23:30 -0600

    Add project ideas and a function to count the words in a given line.

--- whatsapp-parser/readInputFile.hs
+++ whatsapp-parser/readInputFile.hs
@@ -1,9 +1,18 @@
 import System.IO
 import System.Environment
 
+import Data.Char (toLower)
+import Data.List (sort, group)
+import Control.Arrow ((&&&))
+
+wordCount :: String -> [(String, Int)]
+wordCount = map (head &&& length) . group . sort . words . map toLower
+
 main = do
     [fileName] <- getArgs
     fileContent <- readFile fileName
     putStrLn fileContent
     let lineCount = length (lines fileContent)
     putStrLn ("Total messages: " ++ show lineCount)
+    let a = wordCount ((lines fileContent) !! 0)
+    putStrLn (show a)
