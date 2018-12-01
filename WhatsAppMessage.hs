0bc8eb67cc8cd3c0a537d531131b0e0c119b49b2
ChavaO777 <salvador@orozco.in>
2018-12-01 13:16:28 -0600

    Add the correct parsing of the whatsapp messages.

---
+++ WhatsAppMessage.hs
@@ -0,0 +1,37 @@
+import Data.Time
+import Data.List
+
+data WhatsAppMessage = WhatsAppMessage
+    {
+        timeStamp :: LocalTime,
+        author :: String,
+        text :: String
+    } deriving (Show)
+
+parseTimeStamp :: String -> LocalTime
+parseTimeStamp message = 
+    let
+        timeString = takeWhile(/= ']') message
+        timeFormatString = "%-m/%-d/%y, %-H:%M:%S %p"
+    in
+        parseTimeOrError True defaultTimeLocale timeFormatString (drop 1 timeString) :: LocalTime
+
+
+parseAuthor :: String -> String
+parseAuthor = takeWhile(/= ']')
+
+parseText :: String -> String
+parseText = takeWhile(/= ']') . takeWhile(/= ':')
+
+parseMessage :: String -> WhatsAppMessage
+parseMessage message = WhatsAppMessage 
+    {
+        timeStamp = parseTimeStamp message,
+        author = parseAuthor message,
+        text = parseText message
+    }
+
+parseChat :: [String] -> [WhatsAppMessage]
+parseChat lines = 
+    let parsedLines = lines
+    in map (parseMessage) parsedLines
--- readInputFile.hs
+++ readInputFile.hs
@@ -5,12 +5,24 @@ import Data.Char (toLower)
 import Data.List (sort, group)
 import Control.Arrow ((&&&))
 
-wordCount :: String -> [(String, Int)]
-wordCount = map (head &&& length) . group . sort . words . map toLower
+import Data.Char ( isLetter )
+
+removeNonLetters :: [Char] -> [Char]
+removeNonLetters = filter isLetter 
+
+-- computeWordCount :: String -> [(String, Int)]
+-- computeWordCount = map (head &&& length) . group . sort . words . map toLower
+
+-- displayWordCounts :: [IO String] -> [(String, Int)]
+-- displayWordCounts [str] = computeWordCount str
+-- displayWordCounts [str : strs] = computeWordCount [str]
 
 main = do
+    putStr ("Insert the text file name: ")
     fileName <- getLine
     fileContent <- readFile fileName
     putStrLn fileContent
-    let lineCount = length (lines fileContent)
+    let fileLines = lines fileContent
+    -- displayWordCounts fileLines
+    let lineCount = length fileLines
     putStrLn ("Total messages: " ++ show lineCount)
